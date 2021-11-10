0bd07b30f5372a4e31b695da9a9bd5de7208adc5
nrob92 <nrob42@gmail.com>
2021-11-10 21:49:36 +0000

    work

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Home = props => {
+	const [value, setValue] = useState("");
 
-//create your first component
-const Home = () => {
+	const [listItem, setListItem] = useState([]);
+	const [state, setState] = useState(false);
+	const [count, setCount] = useState(0);
+
+	function deleteButton(index) {
+		const deleted = listItem.filter((element, id) => id !== index);
+		setListItem(deleted);
+		setCount(prevCount => prevCount - 1);
+	}
+
+	function toggle() {
+		setState();
+		if (value !== "") {
+			setCount(count + 1);
+		}
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>Todos</h1>
+			<div className="container">
+				<form>
+					<input
+						className="input"
+						placeholder="Type Here"
+						value={value}
+						onChange={e => setValue(e.target.value)}
+					/>
+					<button
+						onClick={e => {
+							e.preventDefault();
+							if (value === "") {
+								/*es lint*/
+							} else {
+								setListItem([...listItem, value]);
+							}
+							toggle();
+							setValue("");
+						}}>
+						Click Me
+					</button>
+				</form>
+				<div>
+					{listItem.map((item, index) => (
+						<li key={index}>
+							{item}
+							<button
+								onClick={() => deleteButton(index)}
+								className="deleteButton">
+								X
+							</button>
+						</li>
+					))}
+				</div>
+				{count > 0 && <li>{count}</li>}
+			</div>
 		</div>
 	);
 };
